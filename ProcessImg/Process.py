261dcf199f11b978ba72b6c4dcb060d62b80243b
nthornet <elcoco42@gmail.com>
2020-06-02 11:02:39 -0500

    Update Process.py

--- ProcessImg/Process.py
+++ ProcessImg/Process.py
@@ -1,13 +1,46 @@
 import image_slicer
 from PIL import ImageDraw, ImageFont, Image
+import copy
 
-im = Image.open('490149_905766.jpg')
+
+def ProcesarImagen(filepath,width, height,cellsize):
+    # cargar imagen
+    img = Image.open(filepath)
+
+    # Cambiar tamano de la imagen
+    imgresize = changeImageSize(width, height, img)
+
+    # Darle un canal Aplha
+    imgresize = imgresize.convert("RGBA")
+    imgresize.save(filepath, 'PNG')
+
+    # Corta la imagen
+    tiles = image_slicer.slice(filepath, number_tiles=None, col=int(width/cellsize), row=int(height/cellsize), save=False)
+
+    # Mostrar los corte *Solo para debugeo*
+    # for tile in tiles:
+    #    tile.image.show()
+
+    return tiles
 
 
-tiles = image_slicer.slice('490149_905766.jpg', 4, save=False)
-for tile in tiles:
-    overlay = ImageDraw.Draw(tile.image)
-    overlay.text((5, 5), str(tile.number), (255, 255, 255), ImageFont.load_default())
-    overlay.show()
+def changeImageSize(maxWidth,
+                    maxHeight,
+                    image):
+    widthRatio = maxWidth / image.size[0]
+    heightRatio = maxHeight / image.size[1]
 
-im.show()
+    newWidth = int(widthRatio * image.size[0])
+    newHeight = int(heightRatio * image.size[1])
+
+    newImage = image.resize((newWidth, newHeight))
+    return newImage
+
+
+if __name__ == "__main__":
+    newcut = ProcesarImagen('../Pygameoflife/490149_905766.jpg',800, 600,100)
+    viejo = copy.deepcopy(newcut)
+    tempimg = newcut[39]
+    tempimg.image.putalpha(200)
+    tempimg.image.show()
+    viejo[39].image.show()
