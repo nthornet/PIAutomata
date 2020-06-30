04dca766f9d14ca2c5758316792da037b04c69f7
Enrique720 <enrique.sobrados@utec.edu.pe>
2020-06-30 11:40:56 -0500

    with cel entitie

--- Pygameoflife/entities.py
+++ Pygameoflife/entities.py
@@ -1,8 +1,8 @@
 class Celula():
-    def __init__(self,isAlive,cordX,cordY,path):
-        self.isAlive = isAlive
+    def __init__(self,cordX,cordY,img,path):
         self.cordX = cordX
         self.cordY = cordY
+        self.img = img
         self.path = path
     
 #class Automata():
