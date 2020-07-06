022a050a280d0861ba86776964c419fae1bc5e00
johantv <johan.tanta@utec.edu.pe>
2020-07-05 19:23:29 -0500

    saved.

--- Entities/entities.py
+++ Entities/entities.py
@@ -3,13 +3,24 @@ sys.path.append('../Image/ProcessImg/')
 
 from Process import ProcesarImagen
 
+class ImageTop:
+    # Automatas(GameOfLife) con Pygame
+
+
 class GameOfLife():
-    def __init__(self, name = str,  filepath, width, height):
+    def __init__(self, name,  filepath, width, height, cellsize):
         self.name = name
-        self.life_dict = GetLife_Dict(filepath, width, height)
+        self.life_dict = self.GetLife_Dict(filepath, width, height, cellsize)
     
-    def GetLife_Dict(filepath, width, height):
-        tiles = ProcesarImagen(filepath, '../Image/CutImg/' + self.name, width, height)
+    def GetLife_Dict(self, filepath, width, height, cellsize):
+        tiles = ProcesarImagen(filepath, '../Image/CutImg/', width, height, cellsize)
+        life_dict = {}
+        for x in range( int(height/cellsize) ):
+            for y in range( int(width/cellsize) ):
+                life_dict[x,y] = Celula(False, tiles[x*width+y])
+
+
+        return tiles
 
     def RunStep():
         pass
@@ -18,3 +29,6 @@ class Celula():
     def __init__(self, alive, sliceImg):
         self.alive = alive
         self.slice = sliceImg 
+
+if __name__ == '__main__':
+    Automata_1 = GameOfLife("Turismo", "../Image/TestImg/test.jpg",  600,600,100)
