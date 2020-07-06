081e8fe3bb57ace8f2d25990b5858dc0d4ef94ac
Enrique720 <enrique.sobrados@utec.edu.pe>
2020-07-05 20:08:24 -0500

    pygame class

--- Entities/entities.py
+++ Entities/entities.py
@@ -1,10 +1,51 @@
-import sys
+import sys,pygame
+from pygame.locals import *
 sys.path.append('../Image/ProcessImg/')
-
 from Process import ProcesarImagen
 
-class ImageTop:
-    # Automatas(GameOfLife) con Pygame
+WHITE = (255, 255, 255)
+
+class Display():
+    def __init__(self,WIDTH,HEIGHT, GameT, GameB):
+        pygame.init() 
+        pygame.display.set_caption('Swarm Intelligence Game of Life')
+        self.screen = pygame.pygame.set_mode(WIDTH,HEIGHT)
+        self.clock = pygame.time.Clock()
+        self.Automatas = []
+        Automatas.append(GameT,GameB) 
+   
+    def PlayGame(self): 
+        pygame.display.update()
+        while True:  # main loop that runs the game
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    pygame.quit()
+                    sys.exit()
+                self.Automatas[0].RunStep()
+            self.COUNT += 1
+        self.screen.fill(WHITE)
+        for item in self.Automatas[0].life_dict:
+            Colorize(item)
+        #drawGrid()
+        pygame.display.update()
+        self.clock.tick(40)
+
+    def Colorize(self,item):
+        x = item[0]
+        y = item[1]
+        
+        cel = self.Automatas[0].getCel(x,y)
+        image = cel.slice
+        
+        coordinates = cel.slice.coords
+        coordinates = (coordinates[1], coordinates[0])
+        
+        if self.Automatas[0].life_dict[item] == 0:
+            image.set_alpha(0)
+        elif self.Automatas[0].life_dict[item] == 1:
+            image.set_alpha(255)
+        self.screen.blit(image, coordinates)
+        return None
 
 
 class GameOfLife():
