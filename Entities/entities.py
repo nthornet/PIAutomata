5dcc81ff50b59c35103443e37b45400546ec31bc
johantv <johan.tanta@utec.edu.pe>
2020-07-09 21:38:59 -0500

    Main function and details in the classes

--- Entities/entities.py
+++ Entities/entities.py
@@ -2,6 +2,7 @@ import sys,pygame
 import random
 import os
 from shutil import rmtree
+from copy import copy
 from pygame.locals import *
 try:
     from Process import ProcesarImagen
@@ -15,48 +16,15 @@ GREEN = (0, 255, 0)
 DARKGREY = (40, 40, 40)
 
 class Display():
-    def __init__(self, width, height, cellsize):
+    def __init__(self, width, height, cellsize, screen, listAutomatas = []):
         self.Width = width
         self.Height = height
         self.Cell = cellsize
-        self.Automatas = []
-        self.screen = None
-        self.CLOCK = None
+        self.Automatas = listAutomatas
+        self.screen = screen
 
     def AddAutomata(self, Automata):
-        self.Automatas.append(Automata)
-
-    def DrawGrid(self):
-        for automata in self.Automatas:                 
-            for item in automata.life_dict.values():
-                item.convertedImage = pygame.image.load(item.slice.filename).convert()
-            automata.Colorize()    
-
-    def PlayGame(self): 
-        pygame.init()
-        pygame.display.set_caption('Swarm Intelligence Game of Life')
-        self.screen = pygame.display.set_mode((self.Width, self.Height))
-        self.CLOCK = pygame.time.Clock()
-
-        self.DrawGrid()
-
-        while True:  # main loop that runs the game
-            self.screen.fill(WHITE)
-            self.PutOnScreen()
-            pygame.display.update()
-            self.RunStep()
-            
-            for event in pygame.event.get():
-                if event.type == QUIT:
-                    pygame.quit()
-                    for automata in self.Automatas:
-                        automata.RemoveImages()
-                    sys.exit()
-                if event.type == KEYDOWN:
-                    for automata in self.Automatas:
-                        automata.initializeLife()
-
-            self.CLOCK.tick(5)
+        self.Automatas.append(Automata) 
 
     def RunStep(self):
         for automata in self.Automatas:
@@ -64,40 +32,24 @@ class Display():
             automata.Colorize()
 
     def PutOnScreen(self):
+        OrderedAutomatons = sorted(self.Automatas, key = lambda automata : automata.Priority, reverse = True)
 
         cell_width = int(self.Width / self.Cell)
         cell_height = int(self.Height / self.Cell)
-
-        if len(self.Automatas) == 1:
-            for item in self.Automatas[0].life_dict:
-                image       = self.Automatas[0].life_dict[item].convertedImage
-                coordinates = self.Automatas[0].life_dict[item].slice.coords
-                self.screen.blit(image, coordinates)
-        else:
-            indice_mayor = -1
-            if self.Automatas[0].Priority > self.Automatas[1].Priority:
-                indice_mayor = 0
-            else:
-                indice_mayor = 1
-
-            indice_menor = abs(indice_mayor-1)
-        
-            for y in range(0, cell_height):
-                for x in range(0, cell_width):
-                        if self.Automatas[indice_mayor].life_dict[x,y].alive == 1:
-                            image       = self.Automatas[indice_mayor].life_dict[x,y].convertedImage
-                            coordinates = self.Automatas[indice_mayor].life_dict[x,y].slice.coords
-                            self.screen.blit(image, coordinates)
-                        else:
-                            image       = self.Automatas[indice_menor].life_dict[x,y].convertedImage
-                            coordinates = self.Automatas[indice_menor].life_dict[x,y].slice.coords
-                            self.screen.blit(image, coordinates)
+        for y in range(0, cell_height):
+            for x in range(0, cell_width):
+                for automata in OrderedAutomatons:
+                    if(automata.life_dict[x,y].alive == 1):
+                        image       = automata.life_dict[x,y].convertedImage
+                        coordinates = automata.life_dict[x,y].slice.coords
+                        self.screen.blit(image, coordinates)
+                        break
 
 class Celula():
-    def __init__(self, alive, sliceImg, convertedImage = None):
+    def __init__(self, alive, sliceImg):
         self.alive = alive
         self.slice = sliceImg
-        self.convertedImage = convertedImage
+        self.convertedImage = pygame.image.load(self.slice.filename).convert()
         
 class GameOfLife():
     def __init__(self, name,  filepath, directory, width, height, cellsize, priority):
@@ -131,36 +83,32 @@ class GameOfLife():
         return self.life_dict[x,y]
 
     def Colorize(self):
-        cell_width = int(self.Width / self.Cellsize)
-        cell_height = int(self.Height / self.Cellsize)
-
-        for y in range(0, cell_height):
-            for x in range(0, cell_width):
-                if self.life_dict[x, y].alive == 0:
-                    self.life_dict[x, y].convertedImage.set_alpha(0)
-                elif self.life_dict[x, y].alive == 1:
-                    self.life_dict[x, y].convertedImage.set_alpha(255)
+        for cell in self.life_dict.values():
+            if(cell.alive == 0):
+                cell.convertedImage.set_alpha(0)
+            elif(cell.alive == 1):
+                cell.convertedImage.set_alpha(255)
 
     def RunStep(self):
-        new_life = {}
+        new_life = copy(self.life_dict)
         for item in self.life_dict:
             neighbour_count = self.GetNeighbours(item)
             if self.life_dict[item].alive == 1:  # cell is alive and we need to check if it will stay alive
                 if neighbour_count < 2:
                     # dies due to underpopulation
-                    new_life[item] = Celula(0, self.life_dict[item].slice, self.life_dict[item].convertedImage)  # 0
+                    new_life[item].alive = 0 
                 elif neighbour_count > 3:
                     # dies due to overcrowding
-                    new_life[item] = Celula(0, self.life_dict[item].slice, self.life_dict[item].convertedImage)  # 0
+                    new_life[item].alive = 0
                 else:
                     # cell stays alive
-                    new_life[item] = Celula(1, self.life_dict[item].slice, self.life_dict[item].convertedImage)  # 1
+                    new_life[item].alive = 1
 
             elif self.life_dict[item].alive == 0:
                 if neighbour_count == 3:
-                    new_life[item] = Celula(1, self.life_dict[item].slice, self.life_dict[item].convertedImage)  # 1
+                    new_life[item].alive = 1
                 else:
-                    new_life[item] = Celula(0, self.life_dict[item].slice, self.life_dict[item].convertedImage)  # 0
+                    new_life[item].alive = 0
         #   print('Run Step')
         self.life_dict = new_life
 
@@ -223,6 +171,8 @@ class GameOfLife():
         self.life_dict[midx+3,midy+3].alive = 1
         self.life_dict[midx+4,midy+3].alive = 1
 
+        self.Colorize()
+
     def DrawSquare(self):
 
         cell_width = int(self.Width / self.Cellsize)
@@ -235,20 +185,52 @@ class GameOfLife():
         self.life_dict[qrtx+1,qrty+1].alive = 1
         self.life_dict[qrtx+2,qrty].alive = 1
         self.life_dict[qrtx+2,qrty+1].alive = 1
+
+        self.Colorize()
     
     def initializeLife(self):
         for cell in self.life_dict.values():
             cell.alive = random.randint(0,1)
+        self.Colorize()
+
+
+def main():
+    WIDTH    = 800
+    HEIGHT   = 600
+    CELLSIZE = 40
+    pygame.init()
+    pygame.display.set_caption('Swarm Intelligence Game of Life')
+    SURFACE = pygame.display.set_mode((WIDTH, HEIGHT))
+    CLOCK = pygame.time.Clock()
+
+    
+    Automata_1 = GameOfLife("Turismo", "../Image/TestImg/test.jpg", '../Image/CutImg/Turismo/', \
+                             WIDTH, HEIGHT, CELLSIZE, 5)
+    Automata_2 = GameOfLife("Turismo", "../Image/TestImg/machu.jpg", '../Image/CutImg/Machu/', \
+                            WIDTH, HEIGHT, CELLSIZE, 10)
+    Top = Display(800, 600, 40, SURFACE)
+    Top.AddAutomata(Automata_1)
+    Top.AddAutomata(Automata_2)
+    Automata_1.initializeLife()
+    Automata_2.initializeLife()
+    while True:  # main loop that runs the game
+        SURFACE.fill(WHITE)
+        Top.PutOnScreen()
+        pygame.display.update()
+        Top.RunStep()
         
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                for automata in Top.Automatas:
+                    automata.RemoveImages()
+                sys.exit()
+            if event.type == KEYDOWN:
+                for automata in Top.Automatas:
+                    automata.initializeLife()
+
+        CLOCK.tick(5)
+
 
 if __name__ == '__main__':
-    Go = Display(800,600,40)
-    Automata_1 = GameOfLife("Turismo", "../Image/TestImg/test.jpg", '../Image/CutImg/Turismo/', \
-                             Go.Width, Go.Height, Go.Cell,5)
-    Automata_2 = GameOfLife("Turismo", "../Image/TestImg/machu.jpg", '../Image/CutImg/Machu/', \
-                             Go.Width, Go.Height, Go.Cell,10)
-    Automata_2.initializeLife()
-    Automata_1.initializeLife()
-    Go.AddAutomata(Automata_1)
-    Go.AddAutomata(Automata_2)
-    Go.PlayGame()
+    main()    
