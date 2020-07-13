27700b33c93ca56758737ba1b0fff03fc13402b3
johantv <johan.tanta@utec.edu.pe>
2020-07-13 16:12:20 -0500

    Filter

--- Image/ProcessImg/Process.py
+++ Image/ProcessImg/Process.py
@@ -2,6 +2,7 @@ import image_slicer
 from PIL import Image
 from os import remove, makedirs
 from shutil import rmtree
+from copy import copy
 
 def ProcesarImagen(filepath, directory, width, height, cellsize):
     # cargar imagen
@@ -26,7 +27,19 @@ def ProcesarImagen(filepath, directory, width, height, cellsize):
     return tiles
 
 if __name__ == "__main__":
-    makedirs('cuting/')
-    newcut = ProcesarImagen('../TestImg/test.jpg', 'cuting/', 600, 600, 100)
-    a = input("orden: ")
-    rmtree('cuting/')
+    img = Image.open('../TestImg/machu.jpg').convert("RGB")
+    width, height = img.size
+    imgRed = copy(img)
+    pixels = imgRed.load() 
+    for py in range(height):
+        for px in range(width):
+            r, g, b = img.getpixel( (px,py) )
+            newr = 0
+            newg = 0
+            newb = b
+            pixels[px,py] = (newr, newg, newb)
+    imgRed.save("azul.png")
+    #makedirs('cuting/')
+    #newcut = ProcesarImagen('../TestImg/test.jpg', 'cuting/', 600, 600, 100)
+    #a = input("orden: ")
+    #rmtree('cuting/')
